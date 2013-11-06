fccc80582c670d11efb159af5a6381cc229cad2c
Vander Gomes <vander.gomes@corp.globo.com>
2013-11-06 11:34:17 -0200

    Refactored view index

--- poll_django/mysite/mysite/polls/views.py
+++ poll_django/mysite/mysite/polls/views.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
-from django.template import Context, loader
+from django.shortcuts import render_to_response
 from mysite.polls.models import Poll
 from django.http import HttpResponse
 
+
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    t = loader.get_template('polls/index.html')
-    c = Context({
-        'latest_poll_list': latest_poll_list,
-    })
-    return HttpResponse(t.render(c))
+    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
     return HttpResponse("You are look at poll %s." % poll_id)
