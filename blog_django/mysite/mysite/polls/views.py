37e13ab892b308537bdf899675362fedef2fea2d
Vander Gomes <vander.gomes@corp.globo.com>
2013-11-06 10:59:48 -0200

    Writing view

--- blog_django/mysite/mysite/polls/views.py
+++ blog_django/mysite/mysite/polls/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+# from mysite.polls.models import Polls
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello! You're at the poll index.")
+
+def detail(request, poll_id):
+    return HttpResponse("You are look at poll %s." % poll_id)
