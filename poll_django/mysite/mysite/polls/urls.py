3666674631bd2b4f07a25bf51322308e21de3daf
Vander Junior <junior@MacBook-Pro-de-Vander.local>
2013-11-10 15:27:12 -0200

    Simplificando e desaclopando as URLSconfs.

---
+++ poll_django/mysite/mysite/polls/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls.defaults import *
+
+
+urlpatterns = patterns('mysite.polls.views',
+    url(r'^$', 'index'),
+    url(r'^(?P<poll_id>\d+)/$', 'detail'),
+    url(r'^(?P<poll_id>\d+)/results/$', 'results'),
+    url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
+)
--- poll_django/mysite/mysite/urls.py
+++ poll_django/mysite/mysite/urls.py
@@ -6,12 +6,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^polls/$', 'mysite.polls.views.index'),
-    url(r'^polls/(?P<poll_id>\d+)/$', 'mysite.polls.views.detail'),
-    url(r'^polls/(?P<poll_id>\d+)/results/$', 'mysite.polls.views.results'),
-    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'mysite.polls.views.vote'),
+    (r'^polls/', include('mysite.polls.urls')),
     # (r'^admin/(.*)', admin.site.root),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 )
